40f4764666608502e053ba3aa0796fd8f55da0a1
Mohit kumar <Bluesky5april2021@gmail.com>
2024-12-27 02:47:44 +0530

    updated overall layout

---
+++ src/app/apiservice.service.ts
@@ -0,0 +1,14 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class APIServiceService {
+
+  // apiUrl = "http://127.0.0.1:5000/"
+  // constructor(private http: HttpClient) { }
+  // getData(){
+  //   return this.http.get(this.apiUrl);
+  // }
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { OnInit } from '@angular/core';
+import { APIServiceService } from './apiservice.service';
 
 @Component({
   selector: 'app-root',
@@ -7,6 +9,12 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+
+  Data:any
+  constructor(private service:APIServiceService){}
+  ngOnInit(): void {
+      // this.service.getData().subscribe()
+  }
   title = 'species-identification';
 }
