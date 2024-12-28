a7db109e2ac7ecfc71515f7203ab72c25b7cc0df
Mohit kumar <Bluesky5april2021@gmail.com>
2024-12-28 22:57:29 +0530

    added routng

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { OnInit } from '@angular/core';
-import { APIServiceService } from './apiservice.service';
+import { HomeComponent } from './home/home.component';
+import { ResourcesComponent } from './resources/resources.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent implements OnInit{
-
-  Data:any
-  constructor(private service:APIServiceService){}
-  ngOnInit(): void {
-      // this.service.getData().subscribe()
-  }
+export class AppComponent{
   title = 'species-identification';
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ResourcesComponent } from './resources/resources.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:"home",component:HomeComponent},
+    {path:"resources",component:ResourcesComponent}
+];
